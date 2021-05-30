7df1dfc2613a04152a41dd97b68b0a6ee4fe2b32
MrHanson <chihanghanson@gmail.com>
2021-05-30 22:34:52 +0800

    feat: tailwind.css in StatusBar

--- components/StatusBar.tsx
+++ components/StatusBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { RiEmotionHappyLine, RiEmotionNormalLine, RiEmotionUnhappyLine } from 'react-icons/ri' 
+import { RiEmotionHappyLine, RiEmotionNormalLine, RiEmotionUnhappyLine } from 'react-icons/ri'
 export interface MineNumProps {
   mineNum: number
 }
@@ -15,43 +15,52 @@ export interface TimerProps {
 export interface StatusBarProps extends MineNumProps, EmotionFaceProps, TimerProps {}
 
 export const MineNumBar: React.FC<MineNumProps> = ({ mineNum }) => {
-  return <div>{mineNum}</div>
+  return (
+    <select name='minenum' id='mine-num' className='h-16 w-16 focus:outline-none'>
+      <option value='30'>30</option>
+      <option value='60'>60</option>
+      <option value='90'>90</option>
+    </select>
+  )
 }
 
 export const EmotionFaceBtn: React.FC<EmotionFaceProps> = ({ emotionFace }) => {
+  let iconCom = null
   if (emotionFace === 'happy') {
-    return (
-      <button>
-        <RiEmotionHappyLine />
-      </button>
-    )
+    iconCom = <RiEmotionHappyLine />
   } else if (emotionFace === 'normal') {
-    return (
-      <button>
-        <RiEmotionNormalLine />
-      </button>
-    )
+    iconCom = <RiEmotionNormalLine />
   } else if (emotionFace === 'unhappy') {
-    return (
-      <button>
-        <RiEmotionUnhappyLine />
-      </button>
-    )
+    iconCom = <RiEmotionUnhappyLine />
   }
 
-  return null
+  const classList = [
+    'w-16',
+    'h-16',
+    'flex',
+    'justify-center',
+    'items-center',
+    'shadow-md',
+    'outline-none',
+    'py-2',
+    'px-4',
+    'rounded-lg',
+    'active:bg-gray-100',
+    'active:shadow-inner',
+  ]
+  return <button className={classList.join(' ')}>{iconCom}</button>
 }
 
 export const TimerBar: React.FC<TimerProps> = ({ time }) => {
-  return <div>{time}</div>
+  return <div className='h-16 w-16 flex justify-center items-center'>{time}</div>
 }
 
 export const StatusBar: React.FC<StatusBarProps> = ({ mineNum, emotionFace, time }) => {
   return (
-    <header>
+    <div className='max-w-screen-md flex justify-between'>
       <MineNumBar mineNum={mineNum} />
       <EmotionFaceBtn emotionFace={emotionFace} />
       <TimerBar time={time} />
-    </header>
+    </div>
   )
 }
